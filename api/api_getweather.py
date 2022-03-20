be3946f323702e8b2ea08259f4185a87b8a16387
Administrator <294166356@qq.com>
2022-03-20 10:02:16 +0800

    修改api文件

--- api/api_getweather.py
+++ api/api_getweather.py
@@ -10,8 +10,10 @@ class ApiWeather(object):
     def get_weather(self, url, apiKey, area, areaID):
         # 定义请求头
         # headers = {"Content-Type": "multipart/form-data"}
+        # headers = {"Content-Type": "application/json"}
         # 定义请求参数data  注意：multipart/form-data与application/json参数格式有区别
         body = MultipartEncoder({"apiKey": apiKey, "area": area, "areaID": areaID})
+        # body = {"apiKey": apiKey, "area": area, "areaID": areaID}
         # 调用get/post方法并返回响应
         return requests.post(url, headers={'Content-Type': body.content_type}, data=body)
-
+        # return requests.post(url, headers=headers, json=body)
